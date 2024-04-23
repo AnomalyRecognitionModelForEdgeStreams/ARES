6a0470a3ce2fde3453e2c463bdaef8e5bee19593
SimoneMungari <simonemungari1@gmail.com>
2024-04-24 01:06:19 +0200

    Added files

---
+++ stream_framework.py
@@ -0,0 +1,166 @@
+import torch
+from tqdm import tqdm
+import numpy as np
+import time
+from sklearn.metrics import roc_auc_score
+
+
+def run_stream(params, times, df, g, model, weights, anomaly_edge_model,
+               anomaly_node_model, device, window_already_seen=100, start_test_index=-1, threshold=0.5):
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    scores_and_labels = []
+
+    model.eval()
+
+    if g.x is not None:
+        x = g.x.to(device)
+    edge_index = g.edge_index.to(device)
+    edge_dict = g.edge_dict
+
+    n_chunk_division = 30
+    time_limit = 100
+
+    if params.dataset == "UNSW-NB15":
+        n_chunk_division = 30
+        time_limit = 30#10
+    elif params.dataset == "ISCX":
+        n_chunk_division = 100
+        time_limit = 15
+    elif params.dataset == "IDS2018":
+        n_chunk_division = 30
+        time_limit = 120
+    elif params.dataset == "CTU-13-Scenario-1":
+        n_chunk_division = 1024
+        time_limit = 200#100
+    elif params.dataset == "CTU-13-Scenario-10":
+        n_chunk_division = 1024
+        time_limit = 200#100
+    elif params.dataset == "CTU-13-Scenario-13":
+        n_chunk_division = 1024
+        time_limit = 250#150
+
+    n_chunk = times.shape[0] // n_chunk_division
+    splitted = np.array_split(times, n_chunk)
+
+    time_track = tqdm(splitted, ncols=0, dynamic_ncols=False)
+    total_time = time.time()
+
+    for epoch, t in enumerate(time_track):
+
+        if start_test_index == -1 and time.time() - total_time > time_limit:
+            return 0, 0, 1, 0, [[0.5, 0], [0.5, 1]], -1
+
+        new_edges = df[df.t.isin(t)][['s', 'd', 'l']].to_numpy()
+        indexes = list(df[df.t.isin(t)].index)
+
+        if epoch % window_already_seen == 0:
+            already_seen = {}
+
+        new_edge_indexes = []
+        for new_edge in new_edges:
+
+            ne_edg = (new_edge[0], new_edge[1])
+
+            if ne_edg not in edge_dict.keys():
+                new_edge_indexes.append(ne_edg)
+                edge_dict[ne_edg] = edge_index.shape[0] - 1 + len(new_edge_indexes)
+
+        if len(new_edge_indexes) > 0:
+            edge_index = torch.cat((edge_index, torch.LongTensor(new_edge_indexes).T.to(device)), axis=1)
+
+        emb = model.encode(x=x, edge_index=edge_index).cpu().detach()
+
+        if params.phi == "mean":
+            edge_embs = (emb[new_edges[:, 0]] + emb[new_edges[:, 1]]) / 2
+        else:
+            edge_embs = (emb[new_edges[:, 1]] - emb[new_edges[:, 0]])
+
+        if type(edge_embs) != np.ndarray:
+            edge_embs = edge_embs.numpy()
+
+        if weights[1] > 0.0:
+            s_embs = emb[new_edges[:, 0]]
+            d_embs = emb[new_edges[:, 1]]
+        labels = new_edges[:, 2]
+
+        if len(edge_embs.shape) == 1:
+            edge_embs = edge_embs.reshape(1, -1)
+            if weights[1] > 0.0:
+                s_embs = s_embs.reshape(1, -1)
+                d_embs = d_embs.reshape(1, -1)
+
+        for e, edge_emb in enumerate(edge_embs):
+            l = labels[e].item()
+            index = indexes[e]
+            ne_edg = (new_edges[e, 0].item(), new_edges[e, 1].item())
+
+            if ne_edg in already_seen.keys():
+                score = already_seen[ne_edg]
+            else:
+
+                features_edge = {}
+                features_s = {}
+                features_d = {}
+                for i in range(params.out_channels):
+                    features_edge[i] = edge_emb[i]
+                    if weights[1] > 0.0:
+                        features_s[i] = s_embs[e][i]
+                        features_d[i] = d_embs[e][i]
+
+                if params.update_hst:
+                    anomaly_edge_model['MinMaxScaler'].learn_one(features_edge)
+                    score_edge = anomaly_edge_model.score_and_learn_one(
+                        features_edge)
+                else:
+                    score_edge = anomaly_edge_model.score_one(features_edge)
+
+                if weights[1] > 0.0:
+
+                    if params.update_hst:
+                        anomaly_node_model['MinMaxScaler'].learn_one(
+                            features_s)
+                        anomaly_node_model['MinMaxScaler'].learn_one(
+                            features_d)
+
+                        score_s = anomaly_node_model.score_and_learn_one(
+                            features_s)
+                        score_d = anomaly_node_model.score_and_learn_one(
+                            features_d)
+                    else:
+                        score_s = anomaly_node_model.score_one(features_s)
+                        score_d = anomaly_node_model.score_one(features_d)
+
+                    score = score_edge * \
+                        weights[0] + score_s * \
+                            weights[1] + score_d * weights[2]
+
+                else:
+                    score = score_edge
+
+                already_seen[ne_edg] = score
+
+            if (start_test_index != -1 and index >= start_test_index) or (start_test_index == -1):
+                if start_test_index == index:
+                    scores_and_labels = np.array(scores_and_labels)
+                    print(roc_auc_score(scores_and_labels[:, 1], scores_and_labels[:, 0]))
+                    scores_and_labels = []
+                    already_seen = {}
+                    total_time = time.time()
+
+            scores_and_labels.append((score, l))
+
+    print("Total time", time.time() - total_time)
+    for score, l in scores_and_labels:
+        if (score > threshold and l == 1):
+            tp += 1
+        elif (score <= threshold and l == 0):
+            tn += 1
+        elif (score > threshold and l == 0):
+            fp += 1
+        elif (score <= threshold and l == 1):
+            fn += 1
+
+    return tp, tn, fp, fn, scores_and_labels, time.time() - total_time
---
+++ utils.py
@@ -0,0 +1,346 @@
+import pandas as pd
+import numpy as np
+import torch
+import os
+from torch_geometric.data import Data
+
+
+def load_dataset(dataset):
+    processed_path = "data/processed/{}".format(dataset)
+    snapshots = []
+
+    for i in range(len(os.listdir(processed_path))):
+        snapshots.append(torch.load("{}/{}.pt".format(processed_path, i)))
+
+    return snapshots
+
+
+def preprocess_dataset(params):
+
+    dataset = params.dataset
+    perc_train = params.perc_train
+    perc_val = params.perc_val
+
+    processed_path = "data/processed/{}".format(dataset)
+
+    if not os.path.exists(processed_path):
+        os.makedirs(processed_path)
+
+    if dataset == "DARPA":
+        path = "data/processed/DARPA/darpa_original.csv"
+        df = pd.read_csv(path, header=None, names=["s", "d", "t", "l"])
+
+        df.l = df.l != '-'
+        df.l = df.l.astype('int')
+
+        all_site = pd.concat([df.s, df.d])
+        all_site = all_site.astype('category')
+        all_site = all_site.cat.codes
+        df.s = all_site[:df.shape[0]]
+        df.d = all_site[df.shape[0]:]
+        df.t = df.t.astype('category')
+        df.t = df.t.cat.codes + 1  # Time starts from 1
+
+        train_chunk = int(df.shape[0] * perc_train)
+        train_chunk_t = df.iloc[train_chunk].t
+
+        val_chunk = int(df.shape[0] * perc_val) + train_chunk
+        val_chunk_t = df.iloc[val_chunk].t
+
+        train = df[df.t <= train_chunk_t]
+        val_and_train = df[df.t <= val_chunk_t]
+        val_and_train = val_and_train[val_and_train['l'] == 0]
+        val = df[(df.t > train_chunk_t) & (df.t <= val_chunk_t)]
+        test = df[df.t > val_chunk_t]
+        val_and_test = df[df.t > train_chunk_t]
+
+        val = val.reset_index()
+        test = test.reset_index()
+
+        max_node = max(df.s.max(), df.d.max()) + 1
+
+        x = torch.arange(max_node,
+                         dtype=torch.float32).reshape(-1,
+                                                      1) / (max_node - 1)
+        x = torch.FloatTensor(
+            [[x_u for _ in range(params.in_channels)] for x_u in x])
+
+        w_train = train[['s', 'd']].value_counts()
+        w_train = w_train.reset_index()
+        w_train['i'] = np.arange(w_train.shape[0])
+        w_train_dict = w_train.set_index(["s", "d"]).to_dict()['i']
+
+        train_graph = Data(x=x, edge_index=torch.LongTensor(
+            w_train[['s', 'd']].to_numpy()).t().contiguous(), edge_dict=w_train_dict)
+
+        w_val = val[['s', 'd']].value_counts()
+        w_val = w_val.reset_index()
+        w_val['i'] = np.arange(w_val.shape[0])
+        w_val_dict = w_val.set_index(["s", "d"]).to_dict()['i']
+
+        val_graph = Data(x=x, edge_index=torch.LongTensor(
+            w_val[['s', 'd']].to_numpy()).t().contiguous(), edge_dict=w_val_dict)
+
+        w_val_and_train = val_and_train[['s', 'd']].value_counts()
+        w_val_and_train = w_val_and_train.reset_index()
+        w_val_and_train['i'] = np.arange(w_val_and_train.shape[0])
+        w_val_and_train_dict = w_val_and_train.set_index(["s", "d"]).to_dict()[
+            'i']
+
+        val_and_train_graph = Data(x=x, edge_index=torch.LongTensor(w_val_and_train[[
+                                   's', 'd']].to_numpy()).t().contiguous(), edge_dict=w_val_and_train_dict)
+
+        return train_graph, val_graph, val_and_train_graph, train, val, test, val_and_test, df
+
+
+def save_ctu(dataset):
+    df = pd.read_csv("data/raw/{}/data.csv".format(dataset))
+
+    df['l'] = df['Label'].apply(
+        lambda x: 1 if x.startswith("flow=From-Botnet") else 0)
+
+    df = df[['SrcAddr', 'DstAddr', 'StartTime', 'l']]
+
+    df['s'] = df['SrcAddr'].astype(str)
+    df['d'] = df['DstAddr'].astype(str)
+    df['t'] = df['StartTime']
+
+    df = df[['s', 'd', 't', 'l']]
+
+    nodes = {}
+    timings = {}
+    count_nodes = 0
+    count_timings = 0
+
+    df = df.sort_values('t')
+    new_df = []
+    for r in df.iloc:
+        if r.s not in nodes.keys():
+            nodes[r.s] = count_nodes
+            nodes[r.s] = count_nodes
+            count_nodes += 1
+
+        if r.d not in nodes.keys():
+            nodes[r.d] = count_nodes
+            count_nodes += 1
+
+        if r.t not in timings.keys():
+            timings[r.t] = count_timings
+            count_timings += 1
+
+        new_df.append([nodes[r.s], nodes[r.d], timings[r.t], r.l])
+
+    df = pd.DataFrame(new_df, columns=['s', 'd', 't', 'l'])
+
+    print("data/processed/{}".format(dataset))
+    if not os.path.exists("data/processed/{}".format(dataset)):
+        os.makedirs("data/processed/{}".format(dataset))
+
+    f = open("data/processed/{}/processed.csv".format(dataset), "w")
+    for t in df.to_numpy():
+        f.write("{},{},{}\n".format(t[0], t[1], t[2]))
+    f.close()
+
+    f = open("data/processed/{}/ground_truth.csv".format(dataset), "w")
+    for t in df.to_numpy():
+        f.write("{}\n".format(t[3]))
+    f.close()
+
+    f = open("data/processed/{}/shape.txt".format(dataset), "w")
+    f.write("{}".format(df.shape[0]))
+    f.close()
+
+    print("Saved!")
+    exit(0)
+
+
+def load_data(params):
+    perc_train = params.perc_train
+    perc_val = params.perc_val
+    in_channels = params.in_channels
+    dataset = params.dataset
+
+    df = pd.read_csv(
+        "data/processed/{}/processed.csv".format(dataset),
+        header=None,
+        names=['s', 'd', 't'])
+    df_gt = pd.read_csv(
+        "data/processed/{}/ground_truth.csv".format(dataset),
+        header=None,
+        names=['l'])
+
+    df['l'] = df_gt['l']
+
+    train_chunk = int(df.shape[0] * perc_train)
+    train_chunk_t = df.iloc[train_chunk].t
+
+    val_chunk = int(df.shape[0] * perc_val) + train_chunk
+    val_chunk_t = df.iloc[val_chunk].t
+
+    train = df[df.t <= train_chunk_t]
+    val_and_train = df[df.t <= val_chunk_t]
+    val_and_train = val_and_train[val_and_train['l'] == 0]
+    val = df[(df.t > train_chunk_t) & (df.t <= val_chunk_t)]
+    test = df[df.t > val_chunk_t]
+    val_and_test = df[df.t > train_chunk_t]
+
+    val = val.reset_index()
+    test = test.reset_index()
+
+    max_node = max(df.s.max(), df.d.max()) + 1
+
+    x = torch.arange(max_node,
+                     dtype=torch.float32).reshape(-1,
+                                                  1) / (max_node - 1)
+    x = torch.FloatTensor([[x_u for _ in range(in_channels)] for x_u in x])
+
+    w_train = train[['s', 'd']].value_counts()
+    w_train = w_train.reset_index()
+    w_train['i'] = np.arange(w_train.shape[0])
+    w_train_dict = w_train.set_index(["s", "d"]).to_dict()['i']
+
+    train_graph = Data(x=x, edge_index=torch.LongTensor(
+        w_train[['s', 'd']].to_numpy()).t().contiguous(), edge_dict=w_train_dict)
+
+    w_val = val[['s', 'd']].value_counts()
+    w_val = w_val.reset_index()
+    w_val['i'] = np.arange(w_val.shape[0])
+    w_val_dict = w_val.set_index(["s", "d"]).to_dict()['i']
+
+    val_graph = Data(x=x, edge_index=torch.LongTensor(
+        w_val[['s', 'd']].to_numpy()).t().contiguous(), edge_dict=w_val_dict)
+
+    w_val_and_train = val_and_train[['s', 'd']].value_counts()
+    w_val_and_train = w_val_and_train.reset_index()
+    w_val_and_train['i'] = np.arange(w_val_and_train.shape[0])
+    w_val_and_train_dict = w_val_and_train.set_index(["s", "d"]).to_dict()['i']
+
+    val_and_train_graph = Data(x=x, edge_index=torch.LongTensor(w_val_and_train[[
+                               's', 'd']].to_numpy()).t().contiguous(), edge_dict=w_val_and_train_dict)
+
+    return train_graph, val_graph, val_and_train_graph, train, val, test, val_and_test, df
+
+
+def save_unsw():
+    columns = [
+        'srcip',
+        "sport",
+        "dstip",
+        "dsport",
+        "proto",
+        "state",
+        "dur",
+        "sbytes",
+        "dbytes",
+        "sttl",
+        "dttl",
+        "sloss",
+        "dloss",
+        "service",
+        "Sload",
+        "Dload",
+        "Spkts",
+        "Dpkts",
+        "swin",
+        "dwin",
+        "stcpb",
+        "dtcpb",
+        "smeansz",
+        "dmeansz",
+        "trans_depth",
+        "res_bdy_len",
+        "Sjit",
+        "Djit",
+        "Stime",
+        "Ltime",
+        "Sintpkt",
+        "Dintpkt",
+        "tcprtt",
+        "synack",
+        "ackdat",
+        "is_sm_ips_ports",
+        "ct_state_ttl",
+        "ct_flw_http_mthd",
+        "is_ftp_login",
+        "ct_ftp_cmd",
+        "ct_srv_src",
+        "ct_srv_dst",
+        "ct_dst_ltm",
+        "ct_src_ltm",
+        "ct_src_dport_ltm",
+        "ct_dst_sport_ltm",
+        "ct_dst_src_ltm",
+        "attack_cat",
+        "Label"]
+
+    df1 = pd.read_csv(
+        "data/raw/UNSW-NB15/UNSW-NB15_1.csv",
+        header=None,
+        names=columns)
+    df2 = pd.read_csv(
+        "data/raw/UNSW-NB15/UNSW-NB15_2.csv",
+        header=None,
+        names=columns)
+    df3 = pd.read_csv(
+        "data/raw/UNSW-NB15/UNSW-NB15_3.csv",
+        header=None,
+        names=columns)
+    df4 = pd.read_csv(
+        "data/raw/UNSW-NB15/UNSW-NB15_4.csv",
+        header=None,
+        names=columns)
+
+    df = pd.concat((df1, df2, df3, df4))
+
+    df = df[['srcip', 'sport', 'dstip', 'dsport', "Ltime", 'Label']]
+
+    # + np.full(df.shape[0], ":") + df['sport'].astype(str)
+    df['s'] = df['srcip'].astype(str)
+    # + np.full(df.shape[0], ":") + df['dsport'].astype(str)
+    df['d'] = df['dstip'].astype(str)
+    df['t'] = df['Ltime']
+    df['l'] = df['Label']
+
+    df = df[['s', 'd', 't', 'l']]
+
+    nodes = {}
+    timings = {}
+    count_nodes = 0
+    count_timings = 0
+
+    df = df.sort_values('t')
+    new_df = []
+    for r in df.iloc:
+        if r.s not in nodes.keys():
+            nodes[r.s] = count_nodes
+            nodes[r.s] = count_nodes
+            count_nodes += 1
+
+        if r.d not in nodes.keys():
+            nodes[r.d] = count_nodes
+            count_nodes += 1
+
+        if r.t not in timings.keys():
+            timings[r.t] = count_timings
+            count_timings += 1
+
+        new_df.append([nodes[r.s], nodes[r.d], timings[r.t], r.l])
+
+    df = pd.DataFrame(new_df, columns=['s', 'd', 't', 'l'])
+
+    f = open("data/processed/UNSW-NB15/processed.csv", "w")
+    for t in df.to_numpy():
+        f.write("{},{},{}\n".format(t[0], t[1], t[2]))
+    f.close()
+
+    f = open("data/processed/UNSW-NB15/ground_truth.csv", "w")
+    for t in df.to_numpy():
+        f.write("{}\n".format(t[3]))
+    f.close()
+
+    f = open("data/processed/UNSW-NB15/shape.txt", "w")
+    f.write("{}".format(df.shape[0]))
+    f.close()
+
+    print("Saved!")
+    exit(0)
